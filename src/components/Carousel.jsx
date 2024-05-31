8c7ac7679ce0e3a9f0332847aa61e2355bcdfb82
RAHUL BANSAL <rahul.bansal.22@outlook.com>
2024-05-31 10:27:35 +0530

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,8 +1,75 @@
 import React, { useState } from "react";
-import carouselData from "../carouselData.json";
 import { FaChevronRight, FaChevronLeft } from "react-icons/fa6";
 import { FaHeart } from "react-icons/fa";
 import './Carousel.css';
+import Image1 from '../assets/i1.jpeg';
+import Image2 from '../assets/i2.jpeg';
+import Image3 from '../assets/i3.jpeg';
+import Image4 from '../assets/i4.jpeg';
+import Image5 from '../assets/i5.jpeg';
+import Image6 from '../assets/i6.jpeg';
+import Image7 from '../assets/i7.jpeg';
+import Image8 from '../assets/i8.jpeg';
+
+const carouselData = [
+  {
+    "id": 1,
+    "image": Image1,
+    "place": "Dubai",
+    "price": "from ₹ 28, 488",
+    "link": "https://www.kayak.co.in/explore/DEL-DXB/20240617,20240620?stops=0"
+  },
+  {
+    "id": 2,
+    "image": Image2,
+    "place": "Mumbai, MH",
+    "price": "from ₹ 9, 667",
+    "link": "https://www.kayak.co.in/explore/DEL-BOM/20240610,20240615?stops=0"
+  },
+  {
+    "id": 3,
+    "image": Image3,
+    "place": "Hyderbad, TG",
+    "price": "from ₹ 9, 634",
+    "link": "https://www.kayak.co.in/explore/DEL-HYD/20240623,20240630?stops=0"
+  },
+  {
+    "id": 4,
+    "image": Image4,
+    "place": "Benaluru",
+    "price": "from ₹ 13, 538",
+    "link": "https://www.kayak.co.in/explore/DEL-BLR/20240607,20240610?stops=0"
+  },
+  {
+    "id": 5,
+    "image": Image5,
+    "place": "Kathmandu",
+    "price": "from ₹ 13,621",
+    "link": "https://www.kayak.co.in/explore/DEL-KTM/20240609,20240616?stops=0"
+  },
+  {
+    "id": 6,
+    "image": Image6,
+    "place": "Chennai",
+    "price": "from ₹ 12, 209",
+    "link": "https://www.kayak.co.in/explore/DEL-MAA/20240610,20240617?stops=0"
+  },
+  {
+    "id": 7,
+    "image": Image7,
+    "place": "Ahmedabad",
+    "price": "from ₹ 8, 139",
+    "link": "https://www.kayak.co.in/explore/DEL-AMD/20240615,20240619?stops=0"
+  },
+  {
+    "id": 8,
+    "image": Image8,
+    "place": "Amristar",
+    "price": "from ₹ 6,811",
+    "link": "https://www.kayak.co.in/explore/DEL-ATQ/20240606,20240610?stops=0"
+  }
+];
+
 
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
